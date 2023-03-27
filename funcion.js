8a7f2d91a207ab380df05e6cc7eb4ee37614fb83
jacobG17 <jacobglez17@gmail.com>
2023-03-26 17:31:43 -0700

    Ya se reconocen las jugadas del poker

--- funcion.js
+++ funcion.js
@@ -1,6 +1,6 @@
 //Arreglo con imagenes
 var lista=[
-"Ac.jpg","2c.jpg","3c.jpg","4c.jpg","5c.jpg","6c.jpg","7c.jpg","8c.jpg","9c.jpg","Dc.jpg","Jc.jpg","Qc.jpg","kc.jpg",
+"Ac.jpg","2c.jpg","3c.jpg","4c.jpg","5c.jpg","6c.jpg","7c.jpg","8c.jpg","9c.jpg","Dc.jpg","Jc.jpg","Qc.jpg","Kc.jpg",
 "Ae.jpg","2e.jpg","3e.jpg","4e.jpg","5e.jpg","6e.jpg","7e.jpg","8e.jpg","9e.jpg","De.jpg","Je.jpg","Qe.jpg","Ke.jpg",
 "Ad.jpg","2d.jpg","3d.jpg","4d.jpg","5d.jpg","6d.jpg","7d.jpg","8d.jpg","9d.jpg","Dd.jpg","Jd.jpg","Qd.jpg","Kd.jpg",
 "At.jpg","2t.jpg","3t.jpg","4t.jpg","5t.jpg","6t.jpg","7t.jpg","8t.jpg","9t.jpg","Dt.jpg","Jt.jpg","Qt.jpg","Kt.jpg"
@@ -15,7 +15,7 @@ function numeroAleatorios(min,max)
 //Funcion para cambiar el mazo
 function CambiarMazo()
 {
-	// Obtener los esta2 de los checkboxes
+	// Obtener los estados de los checkboxes
 	const checkbox1 = document.getElementById("checkCarta1");
 	const checkbox2 = document.getElementById("checkCarta2");
 	const checkbox3 = document.getElementById("checkCarta3");
@@ -122,3 +122,90 @@ function activarcheck(num)
 		document.getElementById("checkCarta" + num).checked = true;
 	}
 }
+
+function EvaluarJugada()
+{
+	//Obtener todas las imágenes por su ID
+	var images = document.querySelectorAll("img");
+
+	// Crear un arreglo vacío para almacenar los nombres
+	var cartas = [];
+
+	// Iterar sobre cada imagen y obtener su nombre sin la extensión ".jpg"
+	images.forEach(function(image) 
+	{
+		var nombre = image.src.split('/').pop().split('.')[0];
+		cartas.push(nombre);
+	});
+
+	// El arreglo 'Cartas' ahora contiene los nombres de las imágenes sin la extensión ".jpg"
+	console.log(cartas);
+
+	// Separa los valores de las cartas y los palos
+	const valores = cartas.map(carta => carta.charAt(0));
+	const palos = cartas.map(carta => carta.charAt(1));
+
+	//Variable de referencia para ordenar los valores en la siguiente funcion flecha
+	const valoresOrdenados = '23456789DJQKA';
+
+	//Ordenar los valores de las cartas de menos a mayor
+	valores.sort((a,b) => {
+		return valoresOrdenados.indexOf(a) - valoresOrdenados.indexOf(b);
+	});
+	console.log(valores)
+
+	//Verifica si hay escalera funcion
+	var esEscalera;
+	if (valores.every((valor, index) => {
+		if (index === 0) {
+		  return true;
+		} else {
+		  const valorAnterior = valores[index - 1];
+		  return valoresOrdenados.indexOf(valor) === valoresOrdenados.indexOf(valorAnterior) + 1;
+		}
+	  })) {
+		esEscalera = true;
+	  } else {
+		esEscalera = false;
+	  }
+ 
+	//Verifica si hay color
+	const esColor = palos.every(palo => palo === palos[0]);
+	console.log(esColor)
+
+	//Verifica la jugada Flor Imperial
+	if (esEscalera && esColor && valores[0] === 'D') 
+	{
+		console.log("Flor imerial")
+	}
+	else if (esEscalera && esColor) {
+		console.log("Escalera de color")
+	}
+	else if (esColor) {
+		console.log("Colorl")
+	} 
+	else if (esEscalera == true) {
+		console.log("Escalera")
+
+	} else if (valores[0] === valores[3] || valores[1] === valores[4]) {
+		console.log("Poker")
+	} 
+	else if ((valores[0] === valores[2] && valores[3] === valores[4]) || (valores[0] === valores[1] && valores[2] === valores[4])) {
+		console.log("Full House")
+	} 
+	else if (valores[0] === valores[2] || valores[1] === valores[3] || valores[2] === valores[4]) {
+		console.log("Tercia")
+	} 
+	else if ((valores[0] === valores[1] && valores[2] === valores[3]) || (valores[0] === valores[1] && valores[3] === valores[4]) || (valores[1] === valores[2] && valores[3] === valores[4])) {
+		console.log("Dos pares")
+	} 
+	else if (valores[0] === valores[1] || valores[1] === valores[2] || valores[2] === valores[3] || valores[3] === valores[4]) {
+		console.log("Par")
+	} 
+	else {
+		console.log("Carta Alta")
+	}
+
+}
+
+
